3ca2a6fb248f113e14cc5396034b238a8f497fdb
sherst <steve.shering@yahoo.co.uk>
2026-05-28 12:27:28 +0100

    Add files via upload

---
+++ src/data/specialities.ts
@@ -0,0 +1,14 @@
+export const specialities = [
+  { value: "general-surgery", label: "General Surgery" },
+  { value: "orthopaedics", label: "Orthopaedics" },
+  { value: "cardiothoracic", label: "Cardiothoracic Surgery" },
+  { value: "neurosurgery", label: "Neurosurgery" },
+  { value: "urology", label: "Urology" },
+  { value: "ent", label: "ENT Surgery" },
+  { value: "ophthalmology", label: "Ophthalmology" },
+  { value: "vascular", label: "Vascular Surgery" },
+  { value: "colorectal", label: "Colorectal Surgery" },
+  { value: "breast", label: "Breast Surgery" },
+  { value: "plastic", label: "Plastic Surgery" },
+  { value: "oral-maxillofacial", label: "Oral & Maxillofacial Surgery" }
+];
