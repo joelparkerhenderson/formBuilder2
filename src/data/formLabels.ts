20a7698ce1b25877415f3370b243fad20dd2dd1e
sherst <steve.shering@yahoo.co.uk>
2026-05-31 02:23:47 +0100

    Significant refactoring

---
+++ src/data/formLabels.ts
@@ -0,0 +1,44 @@
+export const organisationLabels: Record<string, string> = {
+  'aneurin-bevan': 'Aneurin Bevan',
+  'betsi-cadwaladr': 'Betsi Cadwaladr',
+  'cardiff-vale': 'Cardiff & Vale',
+  'cwm-taf': 'Cwm Taf Morgannwg',
+  'hywel-dda': 'Hywel Dda',
+  'powys': 'Powys',
+  'swansea-bay': 'Swansea Bay',
+  'velindre': 'Velindre'
+};
+
+export const specialityLabels: Record<string, string> = {
+  'general-surgery': 'General Surgery',
+  'orthopaedics': 'Orthopaedics',
+  'cardiothoracic': 'Cardiothoracic Surgery',
+  'neurosurgery': 'Neurosurgery',
+  'urology': 'Urology',
+  'ent': 'ENT Surgery',
+  'ophthalmology': 'Ophthalmology',
+  'vascular': 'Vascular Surgery',
+  'colorectal': 'Colorectal Surgery',
+  'breast': 'Breast Surgery',
+  'plastic': 'Plastic Surgery',
+  'oral-maxillofacial': 'Oral & Maxillofacial Surgery'
+};
+
+export const hospitalLabels: Record<string, string> = {
+  'prince-charles': 'Prince Charles Hospital, Merthyr Tydfil',
+  'royal-glamorgan': 'Royal Glamorgan Hospital, Llantrisant',
+  'princess-wales': 'Princess of Wales Hospital, Bridgend'
+};
+
+export const sideLabels: Record<string, string> = {
+  'left': 'Left',
+  'right': 'Right',
+  'bilateral': 'Bilateral',
+  'na': 'Not applicable'
+};
+
+export const yesNoUnknownLabels: Record<string, string> = {
+  'yes': 'Yes',
+  'no': 'No',
+  'unknown': 'Unknown or not recorded'
+};
